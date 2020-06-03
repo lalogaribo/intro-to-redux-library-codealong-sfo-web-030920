45cc79ea77c6ebd38462cf8b403f0e0232e4eb65
Edward Mendoza <lalo.garibo90@gmail.com>
2020-06-03 09:19:51 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -1,35 +1,41 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import './App.css';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import "./App.css";
 
 class App extends Component {
-	handleOnClick = event => {
-		this.props.increaseCount();
-	};
+  handleOnClick = (event) => {
+    this.props.increaseCount();
+  };
 
-	render() {
-		return (
-			<div className="App">
-				<button onClick={this.handleOnClick}>Click</button>
-				<p>{this.props.items.length}</p>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="App">
+        <h3>Welcome {this.props.username}</h3>
+        <button onClick={this.handleOnClick}>Click</button>
+        <p>{this.props.items.length}</p>
+        <input
+          type="text"
+          placeholder="Type new name here..."
+          onChange={this.props.changeName}
+        />
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = state => {
-	return {
-		items: state.items
-	};
+const mapStateToProps = (state) => {
+  return {
+    items: state.items,
+    username: state.name,
+  };
 };
 
-const mapDispatchToProps = dispatch => {
-	return {
-		increaseCount: () => dispatch({ type: 'INCREASE_COUNT' })
-	};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    increaseCount: () => dispatch({ type: "INCREASE_COUNT" }),
+    changeName: (e) =>
+      dispatch({ type: "CHANGE_NAME", payload: { name: e.target.value } }),
+  };
 };
 
-export default connect(
-	mapStateToProps,
-	mapDispatchToProps
-)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -1,16 +1,19 @@
 export default function shoppingListItemReducer(
-	state = {
-		items: []
-	},
-	action
+  state = {
+    items: [],
+    name: "Eduardo",
+  },
+  action
 ) {
-	switch (action.type) {
-		case 'INCREASE_COUNT':
-			return {
-				...state,
-				items: state.items.concat(state.items.length + 1)
-			}
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case "INCREASE_COUNT":
+      return {
+        ...state,
+        items: state.items.concat(state.items.length + 1),
+      };
+    case "CHANGE_NAME":
+      return { ...state, name: action.payload.name };
+    default:
+      return state;
+  }
 }
